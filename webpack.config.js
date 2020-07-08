7e5155769e7c0b5a1c40836c1e1012f62a0b1661
George <g.popov44@icloud.com>
2020-07-08 10:03:27 +0300

    set environment

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'RSS Reader',
+      template: 'template.html',
+      filename: 'index.html',
+    }),
+  ],
+};
