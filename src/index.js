2272880687e71314f29181827537b93ed7fddc96
George <g.popov44@icloud.com>
2020-07-08 19:37:56 +0300

    set environment

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log('Hello World');
+// @ts-check
+
+import init from './init';
+
+init();
--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,6 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'RSS Reader',
       template: 'template.html',
     }),
   ],
