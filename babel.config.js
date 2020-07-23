c84c98f89586330229b1b7b10e74e3b43f43ed28
George <g.popov44@icloud.com>
2020-07-23 23:56:01 +0300

    +refactor-4

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,10 @@ module.exports = {
     ['@babel/preset-env',
       {
         targets: {
-          node: 'current',
+          "edge": "17",
+          "firefox": "60",
+          "chrome": "67",
+          "safari": "11.1",
         },
       },
     ],
