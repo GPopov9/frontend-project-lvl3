8a44edd54bda366ea55789a59793739acb2f86af
George <g.popov44@icloud.com>
2020-08-03 23:08:38 +0300

    refactor-11

--- src/watcher.js
+++ src/watcher.js
@@ -1,20 +1,27 @@
 import onChange from 'on-change';
-import renderValid from './renderValid';
 import renderErrors from './renderErrors';
-import renderProcess from './renderProcess';
+import processStateHandler from './processStateHandler';
 import renderFeeds from './renderFeeds';
 import renderPosts from './renderPosts';
 
 export default (state, input, submitButton, invalid) => onChange(state, (path, value) => {
   switch (path) {
     case 'form.valid':
-      renderValid(value, input, submitButton);
+      /* eslint-disable no-param-reassign */
+      if (value) {
+        input.classList.remove('is-invalid');
+        submitButton.disabled = false;
+      } else {
+        input.classList.add('is-invalid');
+        submitButton.disabled = true;
+      }
       break;
+      /* eslint-enable no-param-reassign */
     case 'form.errors':
       renderErrors(value, invalid);
       break;
     case 'form.process':
-      renderProcess(value, input, submitButton);
+      processStateHandler(value, input, submitButton);
       break;
     case 'data.feeds':
       renderFeeds(state.data.feeds);
